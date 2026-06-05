c2b6e6a7e0fd00f37721b02e8ba13d1e4a15e109
Dan Welling <dwelling@umich.edu>
2026-06-05 14:12:18 -0400

    Finalize labels and clean up code

--- m_compare.py
+++ m_compare.py
@@ -4,12 +4,13 @@
 This script creates a mean forecast by averaging the max db_h/dt values in a
 twenty minutes time intervalfrom the five models used in Pulkkinen et al. 2013.
 Plots comparison of model performance of all five models and the mean, median, and observatios.
-Used as figure 1 in the paper. 
+Used as figure 1 in the paper.
 '''
 
 import numpy as np
 import multimodtools as mmt
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 from spacepy.plot import applySmartTimeTicks
 
@@ -26,6 +27,17 @@ parser.add_argument("-m", "--mag", type=str, default='YKC',
 
 # Process arguments
 args = parser.parse_args()
+
+# Set station names:
+names = {'ABK': 'Abisko', 'PBQ': 'Poste de la Baleine', 'SNK': 'Sanikiluaq',
+         'YKC': 'Yellowknife', 'NEW': 'Newport', 'OTT': 'Ottowa',
+         'WNG': 'Wingst'}
+
+# Set plot style:
+try:
+    plt.style.use('seaborn')
+except OSError:
+    plt.style.use('seaborn-v0_8')
 
 # script options into function arguments?
 tab_kwargs = {'event_set': args.events, 'mag_set': args.mag, 'verbose': False}
@@ -60,30 +72,21 @@ modmedian = np.vstack([t['9_SWMF'].modmax, t['2_LFM-MIX'].modmax, t[
     '4_OPENGGCM'].modmax, t['6_WEIMER'].modmax, t['3_WEIGEL'].modmax])
 modmedian = np.median(modmedian, axis=0)
 
-# plotting
-plt.style.use('seaborn')
-fig = plt.figure(figsize=(10, 8), layout='constrained')
+# TOP PLOT:
+fig = plt.figure(figsize=(10, 7), layout='constrained')
 a1, a2 = fig.subplots(2, 1, sharex=True)
-plt.tick_params(axis='both', labelsize=15)
 
 fig.suptitle(f"Event {tab_kwargs['event_set'][0]} - " +
-             f"{tab_kwargs['mag_set']}", fontsize=20)
-applySmartTimeTicks(a1, plottime)
+             f"{names[tab_kwargs['mag_set']]}", fontsize=20)
 
 # First plot: validation plot - each models max compared to ensemble mean
 for mod in mmt.modnames:
     a1.plot(plottime, t[mod].modmax, c=mmt.cols[mod],
             label=mmt.models[mod], alpha=0.75)
-a1.plot(plottime, modmean, c='black', linestyle='-', label='Ensemble Mean', linewidth=3.5)
-a1.plot(plottime, modmedian, c='grey', linestyle='--', label='Ensemble Median', linewidth=3.5)
+a1.plot(plottime, modmean, c='black', ls='--', label='Ensemble Mean', lw=3.5)
+a1.plot(plottime, modmedian, c='grey', ls='--', label='Ensemble Median', lw=3.5)
 
-
-# a1.text(1 ,0 , 'Ensemble Members')
-
-a1.set_ylabel('$dB_H/dt$ ($nT$)', fontsize=15)
-
-a1.legend(loc='upper right', edgecolor='white', framealpha=1)
-
+# BOTTOM PLOT:
 a2.plot(plottime, t['9_SWMF'].modmax, '-*', c='royalblue', label='SWMF',
         alpha=0.85)
 a2.plot(plottime, t['9_SWMF'].obsmax, '-X', c='crimson', label='Observations',
@@ -91,11 +94,18 @@ a2.plot(plottime, t['9_SWMF'].obsmax, '-X', c='crimson', label='Observations',
 a2.plot(plottime, modmean, '-s', c='black', label='Ensemble Mean')
 a2.plot(plottime, modmedian, '--o', c='grey', label='Ensemble Median')
 
-#a2.text('Ensemble Models', 0.05, 0.95, fontsize=20)
+# Set plot subtitles:
+a1.text(0.01, 0.93, 'Ensemble Members', fontsize=16, transform=a1.transAxes)
+a2.text(0.01, 0.93, 'Ensemble Forecasts', fontsize=16, transform=a2.transAxes)
 
-a2.set_ylabel('$dB_H/dt$ ($nT$)', fontsize=15)
-a2.set_xlabel('Time', fontsize=15)
-
-a2.legend(loc='upper right', edgecolor='white', framealpha=1)
+# Touch up axes:
+for ax in (a1, a2):
+    ax.set_ylabel('$dB_H/dt$ ($nT$)', fontsize=16)
+    ax.legend(loc='upper right', edgecolor='white', framealpha=1)
+    yrange = ax.get_ylim()
+    mult = 2 if yrange[1] - yrange[0] < 5 else 5
+    ax.yaxis.set_major_locator(MultipleLocator(mult))
+    ax.tick_params(axis='both', labelsize=15)
+applySmartTimeTicks(a2, plottime, dolabel=True)
 
 plt.show()
